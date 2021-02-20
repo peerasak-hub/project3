667c84cb585159d7579bb524d12c9447b49c7b21
peerasak-hub <78993119+peerasak-hub@users.noreply.github.com>
2021-02-20 12:04:12 +0700

    homescreen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
+import HomeScreen from "./screen/home_screen";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const addCount = () =>{
-    setCount(count+1);
-  }
+  const addCount = (type = "PLUS") => {
+    if (type === "PLUS") {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div>
+      <HomeScreen></HomeScreen>
       <p> คุณกดปุ่มไปแล้ว {count} ครั้ง </p>
-      <button onClick={addCount}> +1 </button>
+      <button onClick={() => addCount("MINUS")}> -1 </button>
+      <button onClick={() => addCount("PLUS")}> +1 </button>
     </div>
   );
 }
